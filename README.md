918eef3a64c536736b7d0a81c63390fd5ce12718
Antoine Martin <antoine.martin@octave.biz>
2024-06-05 10:27:20 +0200

    🎉 First functionnal commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # newpage-login-sample-js
-Sample JS program to perform an OIDC authentication against New Page
+
+Sample JS program to perform an OIDC authentication against New Page. To perform
+an authentication, please run:
+
+```console
+> npm insall
+> npm start
+```
---
+++ index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import { Issuer, generators } from "openid-client";
+import open from "open";
+import { readFileSync } from "fs";
+
+// Configuration for Azure AD B2C
+// Read the config.json file
+const configData = readFileSync("config.json");
+const config = JSON.parse(configData);
+
+// Extract the variables from the config object
+const { tenantName, clientId, policyName, redirectPort, scope } = config;
+const redirectUri = `http://localhost:${redirectPort}/callback`;
+
+// Generate the code verifier and challenge
+const codeVerifier = generators.codeVerifier();
+const codeChallenge = generators.codeChallenge(codeVerifier);
+
+// Create the Express app
+const app = express();
+
+// Create the Azure AD B2C issuer
+const issuer = await Issuer.discover(
+  `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${policyName}/v2.0/.well-known/openid-configuration`
+);
+
+// Create the client
+const client = new issuer.Client({
+  client_id: clientId,
+  redirect_uris: [redirectUri],
+  response_types: ["code"],
+  token_endpoint_auth_method: "none",
+});
+
+app.get("/callback", async (req, res) => {
+  const params = req.query;
+
+  try {
+    const tokenSet = await client.callback(redirectUri, params, {
+      code_verifier: codeVerifier,
+    });
+    res.send("Login successful! You can close this window.");
+    console.log("Token Set:", tokenSet);
+  } catch (error) {
+    res.send("Error during login process. Please try again.");
+    console.error("Callback Error:", error);
+  }
+
+  process.exit();
+});
+
+app.listen(redirectPort, async () => {
+  console.log(`Server is listening on http://localhost:${redirectPort}`);
+
+  const authUrl = client.authorizationUrl({
+    scope,
+    code_challenge: codeChallenge,
+    code_challenge_method: "S256",
+  });
+
+  console.log("Login URL:", authUrl);
+  await open(authUrl);
+});
